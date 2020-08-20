cce4b28a529273deeb9289679c1d3b0a484a5933
AmadeusGB <1069830494@qq.com>
2020-08-20 19:02:03 +0800

    homework for lesson3-2

---
+++ lesson3/client/target/debug/client.d
@@ -0,0 +1,1 @@
+/home/project/SubstrateClass/lesson3/client/target/debug/client: /home/project/SubstrateClass/lesson3/client/src/main.rs
---
+++ lesson3/server/target/debug/server.d
@@ -0,0 +1,1 @@
+/home/project/SubstrateClass/lesson3/server/target/debug/server: /home/project/SubstrateClass/lesson3/server/src/main.rs
