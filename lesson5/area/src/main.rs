9a879fef416d99879e71b841b0180050ce91cff6
AmadeusGB <1069830494@qq.com>
2020-11-15 08:06:15 +0800

    impl lesson5 for version6

--- lesson5/area/src/main.rs
+++ lesson5/area/src/main.rs
@@ -1,40 +1,46 @@
-use std::ops::Mul;
-struct Round<T: Copy + Mul> {
-    pi: T,
-    r:  T,
+pub trait HasArea {
+    type Output;
+    fn get_area(&self) -> Self::Output;
 }
-struct Triangle<T: Copy + Mul> {
-    b: T,
-    h: T,
+struct Square<T> {
+    side: T,
 }
-struct Rectangle<T: Copy + Mul> {
-    w: T,
-    h: T,
-}
-
-impl<T: Copy + Mul<Output = T>> Round<T> {
-    fn calc_area(&self) -> T {
-        self.pi * self.r * self.r
+impl<T: std::ops::Mul<Output = T> + Copy> HasArea for Square<T> {
+    type Output = T;
+    
+    fn get_area(&self) -> Self::Output {
+        self.side * self.side
     }
 }
-impl<T: Mul<Output = T> + Copy> Triangle<T> {
-    fn calc_area(&self) -> T {
-        self.b * self.h
+struct Circle<T> {
+    radius: T,
+}
+impl<T: std::ops::Mul<Output = T> + Into<f64> + Copy> HasArea for Circle<T> {
+    type Output = f64;
+    
+    fn get_area(&self) -> Self::Output {
+        (self.radius * self.radius).into() * std::f64::consts::PI
     }
 }
-impl<T: Mul<Output = T> + Copy> Rectangle<T> {
-    fn calc_area(&self) -> T {
-        self.w * self.h
+struct Triangle<T> {
+    base: T,
+    hight: T,
+}
+impl<T: std::ops::Mul<Output = T> + Into<f64> + Copy> HasArea for Triangle<T> {
+    type Output = f64;
+    
+    fn get_area(&self) -> Self::Output {
+        (self.base * self.hight).into() * 0.5
     }
 }
-
-
 fn main() {
-    let example1 = Round { pi:3.14, r:10.0};
-    let example2 = Triangle { b:2.0, h:1.0};
-    let example3 = Rectangle { w:2, h:5};
 
-    println!("Round area is {:?}",example1.calc_area());
-    println!("Triangle area is {:?}",example2.calc_area() * 0.5);
-    println!("Rectangle area is {:?}",example3.calc_area());
+    let s = Square {side: 10};
+    println!("square: {}", s.get_area());
+    
+    let r = Circle {radius: 2};
+    println!("circle: {}", r.get_area());
+
+    let t = Triangle {base: 2, hight: 1};
+    println!("Triangle: {}", t.get_area());
 }
