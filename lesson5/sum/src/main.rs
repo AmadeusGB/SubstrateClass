9b6183f89e705ee1ddcae08b570c2e8527845200
AmadeusGB <1069830494@qq.com>
2020-11-16 19:36:23 +0800

    impl lesson5 for version8

--- lesson5/sum/src/main.rs
+++ lesson5/sum/src/main.rs
@@ -5,8 +5,7 @@ fn sum_integer_sets(t: &[u32]) -> Option<u32> {
             match total.checked_add(*item) {
                 Some(s) => total = s,
                 None => return None,
-            }
-            
+            } 
         }
         Some(total)
     }
